b2104cc1a981efcdf2dad7642606b6f9a35a6d66
olehfff <olegsamilenko5@gmail.com>
2024-09-14 19:50:17 +0300

    add : changes

---
+++ localStorage/script.js
@@ -0,0 +1,34 @@
+function getContact (){
+const contact = localStorage.getItem('contact')
+    if(contact) {
+        JSON.parse(contact)
+    }else{
+        return[]
+    }
+}
+console.log(getContact())
+
+function saveContact (){
+const contact = localStorage.setItem().
+JSON.stringify()
+}
+
+function renderContact (){
+    document.getElementById()
+    contactList.innerHTML = '';
+    forEach()
+    document.createElement('li')
+}
+console.log(getContact())
+
+function editContact (){
+
+}
+console.log(getContact())
+
+function deleteContact (){
+    splice()
+    console.log(saveContact())
+    console.log(renderContact())
+}
+console.log(getContact)
