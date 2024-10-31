c1c9153fd7e148259560f99f560d1427ed13731a
olehfff <olegsamilenko5@gmail.com>
2024-10-31 17:25:34 +0200

    add : changes

--- promice/promice.js
+++ promice/promice.js
@@ -1,15 +1,13 @@
 Д/З 1
 
 
-const delay = ms => {
-    
-  };
-  ​
-  const loggerOne = time => console.log(`Resolved after ${time}ms`);
-  ​
-  delay(2000).then(logger); 
-  delay(1000).then(logger); 
-  delay(1500).then(logger);
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const loggerOne = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(() => logger(2000));
+delay(1000).then(() => logger(1000));
+delay(1500).then(() => logger(1500));
 
 
 
@@ -34,57 +32,50 @@ const delay = ms => {
   toggleUserState(users, 'Lux', logger);
   ​
   
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+  toggleUserState(users, 'Mango', loggerTwo);
+  toggleUserState(users, 'Lux', loggerTwo);
 
 
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  const makeTransaction = transaction => {
+    return new Promise((resolve, reject) => {
+      const delay = randomIntegerFromInterval(200, 500);
+    
+      setTimeout(() => {
+        const canProcess = Math.random() > 0.3;
+    
+        if (canProcess) {
+          resolve({ id: transaction.id, time: delay });
+        } else {
+          reject(transaction.id);
+        }
+      }, delay);
+    });
   };
-  ​
-  const makeTransaction = (transaction, onSuccess, onError) => {
-    const delay = randomIntegerFromInterval(200, 500);
-  ​
-    setTimeout(() => {
-      const canProcess = Math.random() > 0.3;
-  ​
-      if (canProcess) {
-        onSuccess(transaction.id, delay);
-      } else {
-        onError(transaction.id);
-      }
-    }, delay);
-  };
-  ​
-  const logSuccess = (id, time) => {
+  
+  const logSuccess = ({ id, time }) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
-  ​
+  
   const logError = id => {
     console.warn(`Error processing transaction ${id}. Please try again later.`);
   };
-  ​
-  makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-  makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-  makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-  makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+  
   makeTransaction({ id: 70, amount: 150 })
     .then(logSuccess)
     .catch(logError);
-  ​
+  
   makeTransaction({ id: 71, amount: 230 })
     .then(logSuccess)
     .catch(logError);
-  ​
+  
   makeTransaction({ id: 72, amount: 75 })
     .then(logSuccess)
     .catch(logError);
-  ​
+  
   makeTransaction({ id: 73, amount: 100 })
     .then(logSuccess)
     .catch(logError);
-
 
 
     Д/З 2
