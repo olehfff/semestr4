3959eac0d5fc2d4be21445409011d2f11984ef36
olehfff <olegsamilenko5@gmail.com>
2025-01-07 20:10:59 +0200

    ajax

--- HTTP-AJAX-DZ/script.js
+++ HTTP-AJAX-DZ/script.js
@@ -1,62 +1,67 @@
-const getInput = document.querySelector('input');
-const getDataContainer = document.querySelector('.data-container');
-console.log(getInput);
-console.log(getDataContainer);
-
-
-
-// import { fetchData } from './fetchCountries' 
-// console.log(fetchData);
-
-
-const countriesApi = 'https://restcountries.com/v3.1/all?fields=name,capital,population,languages,flags';
-async function fetchData(search) {
-    // console.log('hi', countriesApi) 
-    try {
-        const getData = await fetch(countriesApi)
-        console.log(getData)
-        if (!getData.ok) {
-            throw new Error(`could not fetch data ${search}`)
-        }
-        return getData.json()
-    } catch (err) {
-        console.log(err)
+const getInput = document.querySelector('input');  
+const getDataContainer = document.querySelector('.data-container');  
+console.log(getInput);  
+console.log(getDataContainer);  
+  
+  
+  
+// import { fetchData } from './fetchCountries'   
+// console.log(fetchData);  
+  
+  
+const countriesApi = 'https://restcountries.com/v3.1/all?fields=name,capital,population,languages,flags';  
+async function fetchData(search) {  
+    // console.log('hi', countriesApi)   
+    try {  
+        const getData = await fetch(countriesApi)  
+        console.log(getData)  
+        if (!getData.ok) {  
+            throw new Error(`could not fetch data ${search}`)  
+        }  
+        return getData.json()  
+    } catch (err) {  
+        console.log(err)  
+    }  
+}   
+  
+//script.js  
+  
+async function showCountriesData(e) {  
+    const edit = e.target.value.trim().toLowerCase() 
+    if (!edit) { 
+        getDataContainer.innerHTML = "" 
+        return; 
+    } 
+    const getFetchData = await fetchData()  
+    const filterCountries = getFetchData.filter(country => country.name.official.toLowerCase().includes(edit))  
+    if(filterCountries.length === 1){
+        renderCountries(filterCountries[0])
+    } else if(filterCountries.length > 1 && filterCountries.length <= 10) { 
+        renderCountries(filterCountries)
+    } else if(filterCountries.length > 10){
+        getDataContainer.innerHTML = `  
+        <p>Забагато співпадінь, змініть параметр пошуку.</p>`
+    } else {
+        getDataContainer.innerHTML = `  
+        <p>Не знайдено</p>`
     }
 }
-fetchData()
 
-//script.js
-
-async function showCountriesData() {
-    const getFetchData = await fetchData()
-    const inputValue = getInput.value.toLowerCase()
-    if (getFetchData) {
-        const filterCountries = getFetchData.find(country => country.name.official.toLowerCase().includes(inputValue))
-    }
-
+function renderCountries(country) {  
+const { name, flags, capital, population, languages } = country;  
+        const officialName = name.official  
+        const countryFlags = flags.png  
+        const countryLanguages = Object.values(languages).join(', ')  
+        const alt = 'Прапор не знайдено'  
+        getDataContainer.innerHTML = `  
+        <p>${officialName}</p>  
+        <p>${capital}</p>  
+        <p>${population}</p>  
+        <img alt='${alt}' src='${countryFlags}'>  
+        <p>${countryLanguages}</p>  
+    ` 
+} 
+function renderCountriesList(countries){ 
+    getDataContainer.innerHTML=countries.map((country)=>`<p>${country.name.official}</p>`).join('') 
 }
-showCountriesData()
-function renderCountries(country) {
-const { name, flags, capital, population, languages } = country;
-        const officialName = name.official
-        const countryFlags = flags.png
-        const countryLanguages = Object.values(languages).join(', ')
-        const alt = 'Прапор не знайдено'
-        getDataContainer.innerHTML = `
-        <p>${officialName}</p>
-        <p>${capital}</p>
-        <p>${population}</p>
-        <img alt='${alt}' src='${countryFlags}'>
-        <p>${countryLanguages}</p>
-    `
-}
-// const { name, flags, capital, population, languages } = filterCountries;
-//         const officialName = name.official
-//         const countryFlags = flags.png
-//         const countryLanguages = Object.values(languages).join(', ')
-//         getDataContainer.innerHTML = `
-//         <p>${officialName}</p>
-//         <p>${capital}</p>
-//         <p>${population}</p>
-
-//         `
+getInput.addEventListener('input', showCountriesData);
